37cbd155a7da957d61aa17f9527f0f6aafeaeafa
chryl <cyl5yy412@gmail.com>
2021-03-22 11:18:12 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/chryl/RuoyiAdminApplication.java
@@ -0,0 +1,14 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Chr.yl on 2021/3/22.
+ *
+ * @author Chr.yl
+ */
+public class RuoyiAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiAdminApplication.class, args);
+    }
+}
